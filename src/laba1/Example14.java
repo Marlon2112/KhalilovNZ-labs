f19c82d6399fe499465bb99d53bda71cb4f04956
NKHalilov <mongano21@gmail.com>
2024-11-11 17:08:20 +0300

    Example14-fix

--- src/laba1/Example14.java
+++ src/laba1/Example14.java
@@ -3,7 +3,7 @@ package laba1;
 import java.util.Scanner;
 
 public class Example14 {
-    public static void main(String args[]) {
+    public static void main(String... args) {
         Scanner InCMD = new Scanner(System.in);
         System.out.print("Ваш возраст:");
         int age = InCMD.nextInt();
