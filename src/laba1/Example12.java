11276f97c3ef8d18b963dde5cf5233a7ca378345
NKHalilov <mongano21@gmail.com>
2024-11-11 16:52:25 +0300

    Example12

---
+++ src/laba1/Example12.java
@@ -0,0 +1,17 @@
+package laba1;
+
+import java.util.Scanner;
+
+public class Example12 {
+    public static void main(String args[]) {
+        Scanner InCMD = new Scanner(System.in);
+        System.out.print("Ваш год рождения:");
+        int brithyear = InCMD.nextInt();
+        int currentyear;
+        currentyear = 2024;
+        System.out.println("num: " + currentyear);
+       int age = currentyear - brithyear;
+        System.out.println("Ващ возраст: " + age + " лет" );
+        InCMD.close();
+    }
+}
